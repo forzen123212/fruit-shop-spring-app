9f16e6c81eb7a19a23e693ff896739bb51e4b9bb
Hlib Kulikov <forzen123212@proton.me>
2023-12-14 07:57:55 +0200

    JPA REPOSITORY V1

--- src/main/java/com/example/springfirsthw/dto/CreateBookRequestDto.java
+++ src/main/java/com/example/springfirsthw/dto/CreateBookRequestDto.java
@@ -1,13 +1,20 @@
 package com.example.springfirsthw.dto;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import java.math.BigDecimal;
 import lombok.Data;
 
 @Data
 public class CreateBookRequestDto {
+    @NotNull(message = "can't be null")
     private String title;
+    @NotNull(message = "can't be null")
     private String author;
+    @NotNull(message = "can`t be null")
     private String isbn;
+    @NotNull(message = "can`t be null")
+    @Positive(message = "must be more or equals 0")
     private BigDecimal price;
     private String description;
     private String coverImage;
