00566c0485a4eeb45c7179f245ddaab440f1592c
Hlib Kulikov <forzen123212@proton.me>
2023-12-18 12:51:04 +0200

    Handler + some validations

--- src/main/java/com/example/springfirsthw/dto/CreateBookRequestDto.java
+++ src/main/java/com/example/springfirsthw/dto/CreateBookRequestDto.java
@@ -2,16 +2,19 @@ package com.example.springfirsthw.dto;
 
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import java.math.BigDecimal;
 import lombok.Data;
 
 @Data
 public class CreateBookRequestDto {
     @NotNull(message = "can't be null")
+    @Size(min = 1, max = 100)
     private String title;
     @NotNull(message = "can't be null")
     private String author;
     @NotNull(message = "can`t be null")
+    @Size(min = 10, max = 13)
     private String isbn;
     @NotNull(message = "can`t be null")
     @Positive(message = "must be more or equals 0")
