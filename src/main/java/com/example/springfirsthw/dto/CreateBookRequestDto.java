ab811be9abff23311fd8c3453b0f34af7a29e061
Hlib Kulikov <forzen123212@proton.me>
2023-12-22 10:35:51 +0200

    validation improves

--- src/main/java/com/example/springfirsthw/dto/CreateBookRequestDto.java
+++ src/main/java/com/example/springfirsthw/dto/CreateBookRequestDto.java
@@ -1,19 +1,22 @@
 package com.example.springfirsthw.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import jakarta.validation.constraints.Size;
 import java.math.BigDecimal;
 import lombok.Data;
+import org.hibernate.validator.constraints.ISBN;
 
 @Data
 public class CreateBookRequestDto {
+    @NotBlank(message = "can't be blank")
     @Size(min = 1, max = 50)
     private String title;
-    @NotNull(message = "can't be null")
+    @NotBlank(message = "can't be blank!")
     private String author;
     @NotNull(message = "can`t be null")
-    @Size(min = 10, max = 13)
+    @ISBN(message = "wrong format of ISBN")
     private String isbn;
     @Positive(message = "must be more than 1")
     private BigDecimal price;
